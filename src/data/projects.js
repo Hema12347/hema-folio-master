f9e5ad5dc28c43426903f3325073d7fe0a91b15e
hema gopisetty <hemagopisetty2@gmail.com>
2022-02-20 18:46:37 +0530

    first commit

--- src/data/projects.js
+++ src/data/projects.js
@@ -11,7 +11,7 @@ export  const ProjectData=[
         image:"https://images.unsplash.com/photo-1611162616305-c69b3fa7fbe0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80"
     },
     {
-        id:1,
+        id:2,
         title:"Pizza Order Web Application",
         about:"It is a MERN Stack project, which includes the authentication, adding & removing Pizzas from cart, stripe payment gateway and have admin controls.",
         stack:"Stack",
@@ -22,7 +22,7 @@ export  const ProjectData=[
         image:"https://quizizz.com/media/resource/gs/quizizz-media/quizzes/07004ddd-f3d3-421f-bb93-4e6e19da8e0b?w=90&h=90"
     },
     {
-        id:1,
+        id:3,
         title:"WhatsApp-Clone Web Application",
         about:"It is a mern stack project with firebase authentication and includes Pusher  for real time chat and it replicated the UI of whatsapp.",
         stack:"Stack",
@@ -33,7 +33,7 @@ export  const ProjectData=[
         image:"https://images.unsplash.com/photo-1614680376408-81e91ffe3db7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2hhdHNhcHAlMjBsb2dvfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
     },
     {
-        id:1,
+        id:4,
         title:"Car-Booking Web Application",
         about:"It is a MERN Stack project, which includes the authentication, booking car with specific date and time, includes stripe payment gateway and have admin controls.",
         stack:"Stack",
